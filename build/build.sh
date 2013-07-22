8656d5e464d251e93e3d78a197994dbc763bdafe
Thomas Appel <mail@thomas-appel.com>
2013-07-22 23:35:07 +0200

    update composer

--- build/build.sh
+++ build/build.sh
@@ -6,8 +6,9 @@ GIT=`echo $WHICHGIT`
 $GIT subsplit init git@github.com:seleneapp/framework.git
 
 
-$GIT subsplit publish src/Selene/Components/Common:git@github.com:seleneapp/common.git
-$GIT subsplit publish src/Selene/Components/Events:git@github.com:seleneapp/events.git
-$GIT subsplit publish src/Selene/Components/TestSuite:git@github.com:seleneapp/testsuite.git
+$GIT subsplit publish --no-tags src/Selene/Components/Common:git@github.com:seleneapp/common.git
+$GIT subsplit publish --no-tags src/Selene/Components/Events:git@github.com:seleneapp/events.git
+$GIT subsplit publish --no-tags src/Selene/Components/DependencyInjection:git@github.com:seleneapp/dependency-injection.git
+$GIT subsplit publish --no-tags src/Selene/Components/TestSuite:git@github.com:seleneapp/testsuite.git
 
 rm -rf ./.subsplit
