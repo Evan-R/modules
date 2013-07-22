d27585e38db2605294b5b5610ed0af075147db05
Thomas Appel <mail@thomas-appel.com>
2013-07-22 20:45:09 +0200

    fix buildscript

--- build/build.sh
+++ build/build.sh
@@ -9,3 +9,5 @@ $GIT subsplit init git@github.com:seleneapp/framework.git
 $GIT subsplit publish src/Selene/Components/Common:git@github.com:seleneapp/common.git
 $GIT subsplit publish src/Selene/Components/Events:git@github.com:seleneapp/events.git
 $GIT subsplit publish src/Selene/Components/TestSuite:git@github.com:seleneapp/testsuite.git
+
+rm -rf ./.subsplit
