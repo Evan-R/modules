96ee55fdf55207516cd5db0cda3767f5dc0afe30
Thomas Appel <mail@thomas-appel.com>
2013-07-31 18:49:57 +0200

    update composer

--- build/build.sh
+++ build/build.sh
@@ -8,6 +8,7 @@ $GIT subsplit init git@github.com:seleneapp/framework.git
 
 $GIT subsplit publish --no-tags src/Selene/Components/Cache:git@github.com:seleneapp/cache.git
 $GIT subsplit publish --no-tags src/Selene/Components/Common:git@github.com:seleneapp/common.git
+$GIT subsplit publish --no-tags src/Selene/Components/Cryptography:git@github.com:seleneapp/cryptography.git
 $GIT subsplit publish --no-tags src/Selene/Components/Events:git@github.com:seleneapp/events.git
 $GIT subsplit publish --no-tags src/Selene/Components/Filesystem:git@github.com:seleneapp/filesystem.git
 $GIT subsplit publish --no-tags src/Selene/Components/DependencyInjection:git@github.com:seleneapp/dependency-injection.git
--- src/Selene/Components/Cryptography/README.md
+++ src/Selene/Components/Cryptography/README.md
@@ -1,2 +1,2 @@
-# Cryptography and hashing utils
-[![Build Status](https://api.travis-ci.org/streamphp/cryptography.png?branch=master)](https://travis-ci.org/stream/cryptography)
+# De-Encryption, and hashing utils
+[![Build Status](https://api.travis-ci.org/seleneapp/cryptography.png?branch=master)](https://travis-ci.org/seleneapp/cryptography)
