6b4aeea26aae01c5f142a98738f234931261c673
Thomas Appel <mail@thomas-appel.com>
2013-11-22 03:11:14 +0100

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -11,7 +11,7 @@ $GIT subsplit publish --no-tags src/Selene/Components/Common:git@github.com:sele
 $GIT subsplit publish --no-tags src/Selene/Components/Cryptography:git@github.com:seleneapp/cryptography.git
 $GIT subsplit publish --no-tags src/Selene/Components/Events:git@github.com:seleneapp/events.git
 $GIT subsplit publish --no-tags src/Selene/Components/Filesystem:git@github.com:seleneapp/filesystem.git
-$GIT subsplit publish --no-tags src/Selene/Components/DependencyInjection:git@github.com:seleneapp/dependency-injection.git
+$GIT subsplit publish --no-tags src/Selene/Components/DI:git@github.com:seleneapp/dependency-injection.git
 $GIT subsplit publish --no-tags src/Selene/Components/TestSuite:git@github.com:seleneapp/testsuite.git
 $GIT subsplit publish --no-tags src/Selene/Components/Http:git@github.com:seleneapp/http.git
 $GIT subsplit publish --no-tags src/Selene/Components/Routing:git@github.com:seleneapp/routing.git
