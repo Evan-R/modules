ff177de5bce655978bb24e0798d425c715115394
Thomas Appel <mail@thomas-appel.com>
2014-07-31 03:35:35 +0200

    separate apc and apcu driver

---
+++ Cache/Driver/ApcuDriver.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * This File is part of the Selene\Components\Cache\Driver package
+ *
+ * (c) Thomas Appel <mail@thomas-appel.com>
+ *
+ * For full copyright and license information, please refer to the LICENSE file
+ * that was distributed with this package.
+ */
+
+namespace Selene\Components\Cache\Driver;
+
+/**
+ * @class ApcuDriver
+ * @package Selene\Components\Cache\Driver
+ * @version $Id$
+ */
+class ApcuDriver extends ApcDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function cachedItemExists($key)
+    {
+        return apcu_exists($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFromCache($key)
+    {
+        return apcu_fetch($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function writeToCache($key, $data, $expires = 60, $compressed = false)
+    {
+        return apcu_store($key, $data, $expires);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteFromCache($key)
+    {
+        return apcu_delete($key);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function flushCache()
+    {
+        return apcu_clear_cache('user');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function incrementValue($key, $value)
+    {
+        return apcu_inc($key, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function decrementValue($key, $value)
+    {
+        return apcu_dec($key, $value);
+    }
+}
