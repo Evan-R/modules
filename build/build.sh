fe38b8f7de0911b9eea4b1925d366fde069bdfaa
Thomas Appel <mail@thomas-appel.com>
2014-08-15 20:39:09 +0200

    update build script

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 WHICHGIT=`which git`
 GIT=`echo $WHICHGIT`
 
-$GIT subsplit init git@github.com:seleneapp/components.git
+$GIT subsplit init git@github.com:seleneapp/modules.git
 
 
 $GIT subsplit publish --no-tags Cache:git@github.com:seleneapp/cache.git
@@ -13,12 +13,10 @@ $GIT subsplit publish --no-tags Cryptography:git@github.com:seleneapp/cryptograp
 $GIT subsplit publish --no-tags DI:git@github.com:seleneapp/di.git
 $GIT subsplit publish --no-tags Events:git@github.com:seleneapp/events.git
 $GIT subsplit publish --no-tags Filesystem:git@github.com:seleneapp/filesystem.git
-$GIT subsplit publish --no-tags Http:git@github.com:seleneapp/http.git
-$GIT subsplit publish --no-tags Kernel:git@github.com:seleneapp/kernel.git
 $GIT subsplit publish --no-tags TestSuite:git@github.com:seleneapp/testsuite.git
 $GIT subsplit publish --no-tags Routing:git@github.com:seleneapp/routing.git
-$GIT subsplit publish --no-tags Stack:git@github.com:seleneapp/stack.git
 $GIT subsplit publish --no-tags Package:git@github.com:seleneapp/package.git
 $GIT subsplit publish --no-tags Xml:git@github.com:seleneapp/xml.git
+$GIT subsplit publish --no-tags Writer:git@github.com:seleneapp/writer.git
 
 rm -rf ./.subsplit
